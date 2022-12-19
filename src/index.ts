9ae01b73ecce68cd2410eda801b2bd90df8f30fc
Jahanzaib Tayyab <jahanzaib.tayyab@devigital.com>
2022-12-19 15:12:06 +0500

    update student object

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Student } from "./Student.js";
 
-const student = new Student("Zia", "A");
+const student = new Student("Zia", "A", "zia1", "login");
 console.log("🚀 ~ file: index.ts:4 ~ student", student);
 
-const student1 = new Student("Ali", "B");
+const student1 = new Student("Ali", "B", "ali1", "logout");
 console.log("🚀 ~ file: index.ts:7 ~ student1", student1);
